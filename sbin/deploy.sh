5209ed076b2b0d6b0aeefc4df5ca3e4cbb3979aa
Rahul Garg <techy.rahulgarg@gmail.com>
2021-03-03 16:44:27 +0530

    CN4| Rahul + Raja| Fix log file name

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -138,7 +138,7 @@ echo "====Old Raw Data Saver Killed===="
 
 echo "====Deploy Raw Data Saver===="
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataMarseille" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataMarseille" 1>/tmp/raw-station-marseille-data-saver.log 2>/tmp/raw-station-marseille-data-saver.error.log &
 
 nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
 
