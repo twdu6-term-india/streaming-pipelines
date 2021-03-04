3e4915cbf0b5918bd61ab2732dbe936c6cf8b06d
Rahul Garg <techy.rahulgarg@gmail.com>
2021-03-04 09:44:53 +0530

    CN4| Rahul| Add kill for StationDataMarseilleSaverApp

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -133,6 +133,7 @@ echo "====Kill Old Raw Data Saver===="
 kill_application "StationStatusSaverApp"
 kill_application "StationInformationSaverApp"
 kill_application "StationDataSFSaverApp"
+kill_application "StationDataMarseilleSaverApp"
 
 echo "====Old Raw Data Saver Killed===="
 
