5618e286d036ac8913f0d575865a177b52294eec
Karthik N <ext_nkarthik@falabella.cl>
2021-03-05 19:34:41 +0530

    Story#8 |Nitika, Karthik| reset checkpoints locations for all apps

--- zookeeper/seed.sh
+++ zookeeper/seed.sh
@@ -5,32 +5,32 @@ $zk_command create /tw ''
 
 $zk_command create /tw/stationDataNYC ''
 $zk_command create /tw/stationDataNYC/topic station_data_nyc
-$zk_command create /tw/stationDataNYC/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataNYC/checkpointsLocation
+$zk_command create /tw/stationDataNYC/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataNYC/checkpoints
 
 $zk_command create /tw/stationInformation ''
 $zk_command create /tw/stationInformation/kafkaBrokers $kafka_server
 $zk_command create /tw/stationInformation/topic station_information
-$zk_command create /tw/stationInformation/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationInformation/checkpointsLocation
+$zk_command create /tw/stationInformation/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationInformation/checkpoints
 $zk_command create /tw/stationInformation/dataLocation hdfs://$hdfs_server/tw/rawData/stationInformation/data
 
 $zk_command create /tw/stationStatus ''
 $zk_command create /tw/stationStatus/kafkaBrokers $kafka_server
 $zk_command create /tw/stationStatus/topic station_status
-$zk_command create /tw/stationStatus/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationStatus/checkpointsLocation
+$zk_command create /tw/stationStatus/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationStatus/checkpoints
 $zk_command create /tw/stationStatus/dataLocation hdfs://$hdfs_server/tw/rawData/stationStatus/data
 
 $zk_command create /tw/stationDataSF ''
 $zk_command create /tw/stationDataSF/kafkaBrokers $kafka_server
 $zk_command create /tw/stationDataSF/topic station_data_sf
-$zk_command create /tw/stationDataSF/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/checkpointsLocation
+$zk_command create /tw/stationDataSF/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/checkpoints
 $zk_command create /tw/stationDataSF/dataLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/data
 
 $zk_command create /tw/stationDataMarseille ''
 $zk_command create /tw/stationDataMarseille/kafkaBrokers $kafka_server
 $zk_command create /tw/stationDataMarseille/topic station_data_marseille
-$zk_command create /tw/stationDataMarseille/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataMarseille/checkpointsLocation
+$zk_command create /tw/stationDataMarseille/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataMarseille/checkpoints
 $zk_command create /tw/stationDataMarseille/dataLocation hdfs://$hdfs_server/tw/rawData/stationDataMarseille/data
 
 $zk_command create /tw/output ''
-$zk_command create /tw/output/checkpointLocation hdfs://$hdfs_server/tw/stationMart/checkpointsLocation
+$zk_command create /tw/output/checkpointLocation hdfs://$hdfs_server/tw/stationMart/checkpoints
 $zk_command create /tw/output/dataLocation hdfs://$hdfs_server/tw/stationMart/data
