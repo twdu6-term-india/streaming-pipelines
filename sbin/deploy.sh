6ab5e958382f477d566eb4235a53dffd34658481
Karthik N <ext_nkarthik@falabella.cl>
2021-03-05 18:25:12 +0530

    Story#8 |Nitika, Karthik| Switch all consumers new kafka cluster

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -44,11 +44,11 @@ Host bastion.${TRAINING_COHORT}.training
 echo "====SSH Config Updated===="
 
 echo "====Insert app config in zookeeper===="
-scp ./zookeeper/seed.sh kafka.${TRAINING_COHORT}.training:/tmp/zookeeper-seed.sh
-ssh kafka.${TRAINING_COHORT}.training <<EOF
+scp ./zookeeper/seed.sh kafka1.${TRAINING_COHORT}.training:/tmp/zookeeper-seed.sh
+ssh kafka1.${TRAINING_COHORT}.training <<EOF
 set -e
 export hdfs_server="emr-master.${TRAINING_COHORT}.training:8020"
-export kafka_server="kafka.${TRAINING_COHORT}.training:9092"
+export kafka_server="kafka1.${TRAINING_COHORT}.training:9092,kafka2.${TRAINING_COHORT}.training:9092,kafka3.${TRAINING_COHORT}.training:9092"
 export zk_command="zookeeper-shell localhost:2181"
 sh /tmp/zookeeper-seed.sh
 EOF
@@ -139,13 +139,13 @@ echo "====Old Raw Data Saver Killed===="
 
 echo "====Deploy Raw Data Saver===="
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataMarseille" 1>/tmp/raw-station-marseille-data-saver.log 2>/tmp/raw-station-marseille-data-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka1.${TRAINING_COHORT}.training:2181,kafka2.${TRAINING_COHORT}.training:2181,kafka3.${TRAINING_COHORT}.training:2181 "/tw/stationDataMarseille" 1>/tmp/raw-station-marseille-data-saver.log 2>/tmp/raw-station-marseille-data-saver.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g  --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka1.${TRAINING_COHORT}.training:2181,kafka2.${TRAINING_COHORT}.training:2181,kafka3.${TRAINING_COHORT}.training:2181 "/tw/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka1.${TRAINING_COHORT}.training:2181,kafka2.${TRAINING_COHORT}.training:2181,kafka3.${TRAINING_COHORT}.training:2181 "/tw/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka1.${TRAINING_COHORT}.training:2181,kafka2.${TRAINING_COHORT}.training:2181,kafka3.${TRAINING_COHORT}.training:2181 "/tw/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
 
 echo "====Raw Data Saver Deployed===="
 EOF
@@ -174,9 +174,9 @@ echo "====Old Station Consumers Killed===="
 
 echo "====Deploy Station Consumers===="
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory=1g --conf spark.executor.memory=1g  --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.cores.max=1 /tmp/tw-station-consumer_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory=1g --conf spark.executor.memory=1g  --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10 --conf spark.cores.max=1 /tmp/tw-station-consumer_2.11-0.0.1.jar kafka1.${TRAINING_COHORT}.training:2181,kafka2.${TRAINING_COHORT}.training:2181,kafka3.${TRAINING_COHORT}.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationTransformerNYC --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory=1g --conf spark.executor.memory=1g  --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10  --conf spark.cores.max=1 /tmp/tw-station-transformer-nyc_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 1>/tmp/station-transformer-nyc.log 2>/tmp/station-transformer-nyc.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationTransformerNYC --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory=1g --conf spark.executor.memory=1g  --conf spark.sql.streaming.minBatchesToRetain=2 --conf spark.sql.shuffle.partitions=10  --conf spark.cores.max=1 /tmp/tw-station-transformer-nyc_2.11-0.0.1.jar kafka1.${TRAINING_COHORT}.training:2181,kafka2.${TRAINING_COHORT}.training:2181,kafka3.${TRAINING_COHORT}.training:2181 1>/tmp/station-transformer-nyc.log 2>/tmp/station-transformer-nyc.error.log &
 
 echo "====Station Consumers Deployed===="
 EOF
