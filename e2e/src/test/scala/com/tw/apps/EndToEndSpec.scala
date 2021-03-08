69d48b48dfbd948e9d3c9a9f49723b9eab8e0fb2
Balu Abraham Kurian <kurianbalu@gmail.com>
2021-03-08 14:46:02 +0530

    #9 | Agam + Balu | Corrected mock server url, added marseille data in test validation

--- e2e/src/test/scala/com/tw/apps/EndToEndSpec.scala
+++ e2e/src/test/scala/com/tw/apps/EndToEndSpec.scala
@@ -6,9 +6,6 @@ import org.scalatest._
 class EndToEndSpec extends FeatureSpec with Matchers with GivenWhenThen {
 
   feature("End to End spec") {
-
-    import spark.implicits._
-
     val spark = SparkSession
       .builder
       .appName("E2e Test App")
@@ -38,13 +35,15 @@ class EndToEndSpec extends FeatureSpec with Matchers with GivenWhenThen {
         .option("inferSchema", "true")
         .load("hdfs://hadoop:9000" + tempStationMartLocation)
       actualDF.printSchema()
-
+      import spark.implicits._
 
       val expectedDF = Seq(
         (11, 4, true, true, 1602190953, "sf-station-1", "Harmon St at Adeline St", 37.849735, -122.270582),
         (8, 3, true, true, 1602190953, "sf-station-2", "Fountain Alley at S 2nd St", 37.33618830029063, -121.88927650451659),
         (6, 27, true, true, 1614256515, "79", "Franklin St & W Broadway", 40.71911552, -74.00666661),
-        (19, 34, true, true, 1614256515, "72", "W 52 St & 11 Ave", 40.76727216, -73.99392888))
+        (19, 34, true, true, 1614256515, "72", "W 52 St & 11 Ave", 40.76727216, -73.99392888),
+        (7, 2, true, true, 1614257973, "france-station-2", "9207- TEISSEIRE - ROUBAUD", 43.27252367086013, 5.399686062414487),
+        (10, 9, true, true, 1614257973, "france-station-1", "8149-391 MICHELET", 43.25402727813068, 5.401873594694653))
         .toDF("bikes_available", "docks_available", "is_renting", "is_returning",
           "last_updated", "station_id", "name", "latitude", "longitude")
 
