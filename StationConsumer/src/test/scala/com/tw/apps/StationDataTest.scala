f0f092bbe22adab3dc72642ea7c24ef7f7b2184e
Raja Syed <raja.syed@thoughtworks.com>
2021-03-08 16:24:00 +0530

    CN20| Rahul + Raja| Added tests to validate error field

--- StationConsumer/src/test/scala/com/tw/apps/StationDataTest.scala
+++ StationConsumer/src/test/scala/com/tw/apps/StationDataTest.scala
@@ -37,5 +37,44 @@ class StationDataTest extends FeatureSpec with Matchers with GivenWhenThen {
 
       stationData.isValid shouldBe false
     }
+
+    scenario("StationData with error column when having invalid input") {
+      val stationData = StationData(
+        bikes_available = None,
+        docks_available = Some(1),
+        is_renting = true,
+        is_returning = true,
+        last_updated = 987654321,
+        station_id = Some("station 1"),
+        name = Some("someone"),
+        latitude = Some(1234567890),
+        longitude = Some(1234567890)
+      )
+
+      val stationDataWithError = stationData.getStationDataWithError
+      stationDataWithError.error should be("Invalid Bikes Available\n")
+    }
+
+    scenario("StationData with error column when having multiple invalid input") {
+      val stationData = StationData(
+        bikes_available = None,
+        docks_available = Some(1),
+        is_renting = true,
+        is_returning = true,
+        last_updated = 987654321,
+        station_id = None,
+        name = Some("someone"),
+        latitude = None,
+        longitude = Some(1234567890)
+      )
+
+      val stationDataWithError = stationData.getStationDataWithError
+      val expectedErrorMessage =
+        """Invalid Bikes Available
+           |Invalid Station Id
+           |Invalid Latitude
+           |""".stripMargin
+      stationDataWithError.error should be(expectedErrorMessage)
+    }
   }
 }
